1d8bb1e5d0cee681115c45c992beac2826812da2
Steve Marshall <smarshall54@gmail.com>
2017-06-05 04:04:36 -0400

    added startup script with all important course environment resources

---
+++ classenv.sh
@@ -0,0 +1,8 @@
+xpdf /home/steve/Downloads/PracticalProgramming
+
+conda start environment LTPclass
+(enables python 3.4)
+
+github page https://github.com/smarshall54/Learn-to-Program-Coursera-Py
+
+coursera page https://www.coursera.org/learn/learn-to-program/home/week/1
