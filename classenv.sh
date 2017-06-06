4b84a5e1ca4308e76df3ce9f116e5891aff60f4e
Steve Marshall <smarshall54@gmail.com>
2017-06-06 04:32:30 -0400

    week1 and 2 notes and files

--- classenv.sh
+++ classenv.sh
@@ -1,11 +1,19 @@
+# TEXTBOOK
 xpdf /home/steve/Downloads/PracticalProgramming
 
+# PYTHON ENVIRONMENT
 conda start environment LTPclass
 (enables python 3.4)
 
+# CLASS GIT REPO
 github page https://github.com/smarshall54/Learn-to-Program-Coursera-Py
 
+# CLASS MAIN PAGE
 coursera page https://www.coursera.org/learn/learn-to-program/home/week/1
 
+# CLASS NOTEBOOKS
 jupyter notebook /home/steve/git/Learn-to-Program-Coursera-Py/LTP_Course_Notes.ipynb
 jupyter notebook /home/steve/git/Learn-to-Program-Coursera-Py/syntax_practice_jupyter.ipynb
+
+# PYTHON VISUALIZER
+https://www.coursera.org/learn/learn-to-program/supplement/ifEep/resources
